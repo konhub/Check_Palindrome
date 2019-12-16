652b52738bc59ff89fd52cd3a3ab2f88b630a529
Konrad Wila <55399835+konhub@users.noreply.github.com>
2019-12-16 10:41:43 +0000

    English and user input added

--- Check_Palindrome.vb
+++ Check_Palindrome.vb
@@ -1,19 +1,22 @@
-﻿Module Module1
-    'Skapad av Markus Nordin
-    'Kolla om en sträng är Palindrome. Alltså stavas samma åt båda hållen som racecar
+Module Module1
+    'Created by Konrad Wila
+    'Checks if a string is Palindrome. So the same is spelled in both directions for example a racecar
     Sub Main()
         Dim testString As New List(Of String)
         Dim word As String = ""
+        Dim input As String
         Dim Buffer() As Char = ""
         Dim MaxWords As Integer = 0
         Dim AntalPalinDrome As Integer = 0
-        testString.Add("palindrome")
-        testString.Add("racecar")
-        testString.Add("anna")
-        testString.Add("volvo")
-        testString.Add("radar")
+
+        Do
+            Console.WriteLine("Enter the words/numbers to be checked for being a palindrome")
+            input = Console.ReadLine()
+            testString.Add(input)
+        Loop Until input = ""
+
         MaxWords = testString.Count
-        Console.WriteLine("Kollar igenom en lista av ord" & vbNewLine)
+        Console.WriteLine("Look through a list of words" & vbNewLine)
         For Each word In testString
             Buffer = word
             Array.Reverse(Buffer)
@@ -23,8 +26,8 @@
             End If
         Next
 
-        Console.WriteLine(vbNewLine & "Hittade " & AntalPalinDrome & " palindromes" & vbNewLine)
-        Console.WriteLine("Tryck på en knapp för att fortsätta")
+        Console.WriteLine(vbNewLine & "Found " & AntalPalinDrome & " palindromes" & vbNewLine)
+        Console.WriteLine("Press a button to exit")
         Console.ReadKey()
     End Sub
 
